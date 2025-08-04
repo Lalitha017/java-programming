84af06241a0788130b3f287e78e68ccf7964d520
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-04 12:26:36 +0530

    Create statement.java

---
+++ statement.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        int a=20;
+        int b=19;
+        if (a<b){
+            System.out.println("a is younger");
+        }
+        else{
+            System.out.println("b is younger");
+        }
+            
+            
+            
+                                            }
+        }

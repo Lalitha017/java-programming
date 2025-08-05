c98cfceb8df40b3d5b840b5977950c3c615c21cd
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-05 11:47:41 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner data = new Scanner(System.in);
+        System.out.println("enter the number");
+        num = data.nextInt();
+        for(int count = 1;count<=10;count++)
+        {
+            System.out.println(count);
+        }
+    }
+}

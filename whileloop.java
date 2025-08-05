007a0f4cc3659687610b2847e9a74b5f9a1f47f9
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-05 10:28:22 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner data = new Scanner(System.in);
+           System.out.println("enter the number");
+           num=data.nextInt();
+        while(num>=0){
+            if(num%2==0){
+                System.out.println("even");
+                       }
+            else{
+               System.out.println("odd");
+               }
+            
+                      }
+                                        }
+                }
+
+        
+           

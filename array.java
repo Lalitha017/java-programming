2f7a77c5899882918cb9e5877a962797b24eca82
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-06 10:12:21 +0530

    Update array.java

--- array.java
+++ array.java
@@ -5,3 +5,50 @@ class Main {
         System.out.println("array[1]");
     }
 }
+
+
+IF FOR
+class Main {
+    public static void main(String[] args) {
+        String[] array = {"abc", "efg", "hig"};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals("efg")) {
+                array[i] = "abc";
+                System.out.println("Updated value: " + array[i]); 
+            }
+            System.out.println("Value at index " + i + ": " + array[i]);
+            System.out.println("Second element: " + array[1]);
+        }
+    }
+}
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        String[] array = {"aaa", "bbb", "ccc", "ddd", "eee", "fff"};
+        array[1] = "aaa"; 
+        System.out.println(array[1]); 
+        
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+            
+            if (array[i].equals("ccc")) {
+                array[i] = "eee";
+            }
+        }
+
+        System.out.println("Updated array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
+
+
+       
+
+       
+
+
+    

88fd09127bb501f814ca1a7abb72f8de8200ccd0
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-06 12:13:55 +0530

    Update array.java

--- array.java
+++ array.java
@@ -45,6 +45,28 @@ class Main {
     }
 }
 
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt(); 
+        int[] arr = new int[size]; 
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt(); 
+        }
+        System.out.println("Array elements are:");
+        for (int num : arr) {
+            System.out.println(num); 
+        }
+        scanner.close(); 
+    }
+}
+
+
+
+
 
        
 

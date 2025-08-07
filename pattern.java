446a03106e89d0f2bf27f71792b7661033a3c359
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-07 12:19:13 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -87,3 +87,30 @@ public class Main {
     }
 }
 
+public class DoublePyramid {
+    public static void main(String[] args) {
+        int n=6; 
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n-i;j++) 
+            {
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++) 
+            {
+                System.out.print("* ");
+            }
+
+            for (int j=1;j<=(n-i)*2;j++) 
+            {
+                System.out.print(" ");
+            }
+
+            for (int k=1;k<=i;k++) 
+            {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}

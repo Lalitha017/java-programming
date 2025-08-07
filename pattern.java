dd1ef3c2ebcfad80d964e2ebeb23ccf3d92cf847
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-07 12:10:25 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -9,3 +9,51 @@ public class Main {
         }
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "QWERTY";
+        int n = name.length();
+        
+        
+        for (int i = 1; i <= n; i++) {
+            
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+
+            
+            for (int j = 0; j < i; j++) {
+                System.out.print(name.charAt(j) + " ");
+            }
+
+            
+            for (int j = i - 2; j >= 0; j--) {
+                System.out.print(name.charAt(j) + " ");
+            }
+
+            System.out.println();
+        }
+
+        
+        for (int i = n - 1; i >= 1; i--) {
+            
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+
+           
+            for (int j = 0; j < i; j++) {
+                System.out.print(name.charAt(j) + " ");
+            }
+
+            
+            for (int j = i - 2; j >= 0; j--) {
+                System.out.print(name.charAt(j) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+

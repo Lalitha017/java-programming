d584e7b9859509cefc396d9fb08c63286a0182d3
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-08 09:40:51 +0530

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,10 @@
+//methods
+class Main {
+    public static void main(String[] args) {
+        myMethod("hello world");
+    }
+
+    public static void myMethod(String name) {
+        System.out.println(name);
+    }
+}

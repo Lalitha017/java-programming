6079e2d25fe9884b1ec63e11cc9ce9f3c1be6c0b
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-11 12:14:01 +0530

    Update formatrix.java

--- formatrix.java
+++ formatrix.java
@@ -160,3 +160,21 @@ class Main {
         }
     }
 }
+//
+class Main {
+    public static void main(String[] args) {
+        int[][][] arr = {
+            { {1, 2, 3} },                         
+            { {4, 5, 6}, {7, 8, 9}, {10, 11, 12} }  
+        };
+        for (int[][] twoD : arr) {       
+            for (int[] oneD : twoD) {    
+                for (int num : oneD) {  
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+            }
+            System.out.println(); 
+        }
+    }
+}

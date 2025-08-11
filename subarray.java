ec8aa07af4ff67cf0df353d1791768e56e4b1583
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-11 15:42:22 +0530

    Create subarray.java

---
+++ subarray.java
@@ -0,0 +1,32 @@
+//subareay
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int a = 3;
+        int b = 4;
+        int[] sub = new int[b - a];
+        for (int i = a; i < b; i++) {
+            sub[i - a] = arr[i];
+        }
+        System.out.println(Arrays.toString(sub));  
+    }
+}
+
+//merge
+class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 0, 3};
+        int[] arr2 = {4, 5, 7};
+        int[] merged = new int[arr1.length + arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            merged[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            merged[arr1.length + i] = arr2[i];
+        }
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+    }
+}

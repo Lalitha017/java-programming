fc9baa937e53a50caa88b933c94e53df2f0b7da7
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-11 11:40:16 +0530

    Update formatrix.java

--- formatrix.java
+++ formatrix.java
@@ -100,3 +100,25 @@ class Main {
         }
     }
 }
+//
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in); 
+        int row = x.nextInt(); 
+        int col = x.nextInt();
+        int[][] arr = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = x.nextInt(); 
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

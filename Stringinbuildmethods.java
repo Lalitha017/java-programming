3cc97fa9f6ba160f319bfe52ca2b688c7861cba2
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-12 10:19:56 +0530

    Update Stringinbuildmethods.java

--- Stringinbuildmethods.java
+++ Stringinbuildmethods.java
@@ -38,3 +38,37 @@ class Main {
         System.out.println("The After Character is:"+ch2);
     }
 }
+//codePointCount
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String str = data.nextLine(); 
+        int codePointCount = str.codePointCount(0, str.length()); 
+        System.out.println("The Unicode code point count is: " + codePointCount);
+    }
+}
+//compareTo
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.println("Enter Name1:");
+        String str = data.nextLine();
+        System.out.println("Enter Name2:");
+        String str1=data.nextLine();
+        char ch = str.charAt(0);
+        System.out.println("The Character is: " + ch);
+        char ch1 = (char) str.codePointBefore(3);
+        System.out.println("The Character Before is: " + ch1);
+        char ch2 = (char) str.codePointAt(4);
+        System.out.println("The Character After is: " + ch2);
+        int ch3 = str.codePointCount(0, 4);
+        System.out.println("The number is: " + ch3);
+        int ch4=str.compareTo(str1);
+        System.out.println("Result:"+ch4);
+        
+       
+        
+        

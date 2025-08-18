2f6b86d19c78c75c4f5b3fcf82ea108f84e4c9af
Lalitha017 <lalithameenakshi9107@mountzion.ac.in>
2025-08-18 12:21:09 +0530

    Create oops.java

---
+++ oops.java
@@ -0,0 +1,102 @@
+//
+class Payment {
+    public void payment() {
+        System.out.println("Payment");
+    }
+}
+class UPI extends Payment {
+    @Override
+    public void payment() {
+        System.out.println("UPI Payment");
+    }
+}
+class Banktransfer extends Payment{
+    @Override
+    public void payment() {
+        System.out.println("Bank Transfer Payment");
+    }
+}
+class Creditcard extends Payment{
+    @Override
+    public void payment() {
+        System.out.println("Credit card Payment");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Payment v = new UPI();
+        v.payment();
+        Payment a = new Banktransfer();
+        a.payment();
+        Payment c = new Creditcard();
+        c.payment();
+        Payment b = new Payment();
+        b.payment();
+    }
+}
+
+//
+class Vehicle {
+    public void drive() {
+        System.out.println("drive");
+    }
+}
+class Bike extends Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("driving bike");
+    }
+}
+class Car extends Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("driving car");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Vehicle v = new Bike();
+        v.drive();   
+        Vehicle a = new Car();
+        a.drive();   
+        Vehicle b = new Vehicle();
+        b.drive();
+    }
+}
+
+//
+class EmployeeSalary {
+    public void salary() {
+        System.out.println("Employees Salary");
+    }
+}
+class Manager extends EmployeeSalary{
+    @Override
+    public void salary() {
+        System.out.println("My salary is 100000");
+    }
+}
+class HR extends EmployeeSalary{
+    @Override
+    public void salary() {
+        System.out.println("My salary is 50000");
+    }
+}
+class Employee extends EmployeeSalary{
+    @Override
+    public void salary() {
+        System.out.println("My salary is 30000");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        EmployeeSalary v = new Manager();
+        v.salary();
+        EmployeeSalary a = new HR();
+        a.salary();
+        EmployeeSalary c = new Employee();
+        c.salary();
+        EmployeeSalary b = new EmployeeSalary();
+        b.salary();
+    }
+}
